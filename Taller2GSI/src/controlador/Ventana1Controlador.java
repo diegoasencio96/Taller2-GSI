416e4e5f9f03a9795d2493f2469604eb02301aaa
diegoasencio96 <diegoasencio96@gmail.com>
2018-06-09 09:27:19 -0500

    Creación del proyecto inicial, con la estructura base MVC en JAVA closed #2

---
+++ Taller2GSI/src/controlador/Ventana1Controlador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.Ventana1;
+
+/**
+ *
+ * @author lenovo-G40
+ */
+public class Ventana1Controlador implements ActionListener {
+    
+    private Ventana1 obj;
+    
+    public Ventana1Controlador(Ventana1 obj) {
+        this.obj=obj;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+    
+    }
+    
+}
---
+++ Taller2GSI/src/vista/Ventana1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author lenovo-G40
+ */
+public class Ventana1 extends JFrame {
+    
+    
+    public Ventana1() {
+        
+        setTitle("Taller 2 - GSI");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setResizable(false);
+        setVisible(true);
+    }
+}
